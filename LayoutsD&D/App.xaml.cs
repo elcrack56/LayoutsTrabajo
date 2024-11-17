4f550b1160c9f1de72057557fffa32e15c8c8c8c
Ana Lopez <Ana Lopez@DESKTOP-LMFSS18>
2024-11-17 03:11:44 -0500

    Agregar archivos de proyecto.

---
+++ LayoutsD&D/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LayoutsD_D
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LayoutsD&D/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LayoutsD_D
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
